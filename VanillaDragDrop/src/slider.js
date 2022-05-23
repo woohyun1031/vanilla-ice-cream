b97ef49e382ee41ba7436d9f49ea28c2b317e7c7
Woohyun <ktkwhms3@gmail.com>
2022-05-23 18:48:15 +0900

    Feat: Implement drag slider bar

---
+++ VanillaDragDrop/src/slider.js
@@ -0,0 +1,35 @@
+let thumb = slider.querySelector(".thumb");
+
+thumb.onmousedown = function (event) {
+  event.preventDefault(); // prevent selection start (browser action)
+
+  let shiftX = event.clientX - thumb.getBoundingClientRect().left;
+  // shiftY not needed, the thumb moves only horizontally
+
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+
+  function onMouseMove(event) {
+    let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
+
+    // the pointer is out of slider => lock the thumb within the bounaries
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+    let rightEdge = slider.offsetWidth - thumb.offsetWidth;
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+
+    thumb.style.left = newLeft + "px";
+  }
+
+  function onMouseUp() {
+    document.removeEventListener("mouseup", onMouseUp);
+    document.removeEventListener("mousemove", onMouseMove);
+  }
+};
+
+thumb.ondragstart = function () {
+  return false;
+};
