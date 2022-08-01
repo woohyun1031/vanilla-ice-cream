a348826536c41431897b9215393a7d8b6a14fafc
Woohyun <ktkwhms3@gmail.com>
2022-08-02 00:18:07 +0900

    Docs: drag drop 주석 추가

--- DragDropPractice/BarDragDrop/src/main.js
+++ DragDropPractice/BarDragDrop/src/main.js
@@ -1,6 +1,7 @@
 const isthumb = document.getElementById("thumb")
 const isslider = document.getElementById("slider")
 
+// (0) 브라우저 자체적으로 제공하는이미지나 요소에 대한 드래그 앤 드롭 비활성화
 isthumb.ondragstart = function () {
   return false
 }
@@ -8,14 +9,29 @@ isthumb.ondragstart = function () {
 isthumb.addEventListener("mousedown", (event) => {
   event.preventDefault()
 
+  //pointer의 pick 위치
   let shiftX = event.clientX - isthumb.getBoundingClientRect().left
 
   const onmouseMove = (event) => {
-    console.log(shiftX)
+    //클릭한부분으로부터 남은 slider길이
     let newLeft = event.clientX - shiftX - isslider.getBoundingClientRect().left
-    if (newLeft < 0) newLeft = 0
+    console.log(
+      `event.clientX: ${
+        event.clientX
+      }, shiftX: ${shiftX}, isslider.getBoundingClientRect().left: ${
+        isslider.getBoundingClientRect().left
+      }`
+    )
+    if (newLeft < 0) {
+      console.log(newLeft, "newleft < 0")
+      newLeft = 0
+    }
     let rightEdge = isslider.offsetWidth - isthumb.offsetWidth
-    if (newLeft > rightEdge) newLeft = rightEdge
+    if (newLeft > rightEdge) {
+      console.log(rightEdge, "newleft > rightEdge")
+      newLeft = rightEdge
+    }
+    console.log(newLeft, "thumb left px")
     isthumb.style.left = newLeft + "px"
   }
 
@@ -24,6 +40,7 @@ isthumb.addEventListener("mousedown", (event) => {
     document.removeEventListener("mousemove", onmouseMove)
   }
 
+  //mousedown과 동시에 addEventlistener로 등록
   document.addEventListener("mousemove", onmouseMove)
   document.addEventListener("mouseup", onmouseUp)
 })
